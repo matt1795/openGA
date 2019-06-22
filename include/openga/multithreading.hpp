d55b8ce9483cb1706854cb5665c9bc9c75d27da1
Matthew Knight <mgk1795@gmail.com>
2019-06-22 22:47:16 +0000

    began thread pool implementation

--- include/openga/multithreading.hpp
+++ include/openga/multithreading.hpp
@@ -10,6 +10,7 @@
 #include <array>
 #include <iostream>
 #include <mutex>
+#include <numeric>
 #include <thread>
 #include <vector>
 
@@ -24,8 +25,39 @@ namespace OpenGA {
             std::for_each(first, last, f);
         }
 
-        template <typename OutputId, typename Size, typename Generator>
-        static void generate_n(OutputId first, Size count, Generator g) {
+        template <typename OutputIt, typename Size, typename Generator>
+        static void generate_n(OutputIt first, Size count, Generator g) {
+            std::generate_n(first, count, g);
+        }
+    };
+
+    template <auto num>
+    struct ThreadPool {
+        template <typename InputIt, typename UnaryFunction>
+        static void for_each(InputIt first, InputIt last, UnaryFunction f) {
+            InputIt it = first;
+            std::mutex mtx;
+            std::vector<std::thread> threads(num);
+            for (auto& th : threads) {
+				th = std::thread([&]() {
+					InputIt local;
+					mtx.lock();
+					while (it != last) {
+						local = it;
+						++it;
+						mtx.unlock();
+						f(*local);
+						mtx.lock();
+					}
+				});
+			}
+
+            for (auto& th : threads)
+                th.join();
+        }
+
+        template <typename OutputIt, typename Size, typename Generator>
+        static void generate_n(OutputIt first, Size count, Generator g) {
             std::generate_n(first, count, g);
         }
     };
