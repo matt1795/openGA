083d10111f56b1e7d5f40bd263fa88f56bc65c86
Matthew Knight <mgk1795@gmail.com>
2019-10-31 08:36:24 -0700

    adding thread pool strategy

--- conanfile.py
+++ conanfile.py
@@ -2,12 +2,15 @@ from conans import ConanFile, CMake, tools
 
 class OpenGAConan(ConanFile):
     name = "openga"
-    version = "2.0.1"
+    version = "2.1.0"
+    description = "Modern C++ library for genetic algorithms"
+    url = "https://github.com/matt1795/openga"
     license = "MIT"
     author = "Matthew Knight <mgk1795@gmail.com>"
     generators = "cmake"
     exports_sources = "*"
     settings = "os", "compiler"
+    requirements = "asio/1.13.0@bincrafters/stable"
 
     options = {
         "examples": [True, False]
@@ -16,12 +19,12 @@ class OpenGAConan(ConanFile):
     default_options = {
         "examples": False
     }
-    
+
     def build(self):
         stdAllowed = [ '17', 'gnu17', '20', 'gnu20' ]
         if not self.settings.compiler.cppstd in stdAllowed:
             raise Exception("need atleast C++17")
-        
+
         cmake = CMake(self)
         if self.options.examples:
             cmake.definitions["Examples"] = "ON"
@@ -37,10 +40,3 @@ class OpenGAConan(ConanFile):
     def package_info(self):
         if self.settings.os == "Linux":
             self.cpp_info.libs.append("pthread")
-
-        stdString = "-std="
-        if not str(self.settings.compiler.cppstd).startswith("gnu"):
-            stdString += "c++"
-            
-        stdString += str(self.settings.compiler.cppstd)
-        self.cpp_info.cxxflags.append(stdString)
--- include/openga/multithreading.hpp
+++ include/openga/multithreading.hpp
@@ -6,6 +6,8 @@
 
 #pragma once
 
+#include <asio.hpp>
+
 #include <algorithm>
 #include <array>
 #include <iostream>
@@ -24,9 +26,33 @@ namespace OpenGA {
             std::for_each(first, last, f);
         }
 
-        template <typename OutputId, typename Size, typename Generator>
-        static void generate_n(OutputId first, Size count, Generator g) {
+        template <typename OutputIt, typename Size, typename Generator>
+        static void generate_n(OutputIt first, Size count, Generator g) {
             std::generate_n(first, count, g);
         }
     };
+
+    struct ThreadPool {
+        template <typename InputIt, typename UnaryFunction>
+        static void for_each(InputIt first, InputIt last, UnaryFunction f) {
+            asio::thread_pool thread_pool;
+            auto strand = make_shared(thread_pool);
+            std::for_each(first, last,
+                          strand.wrap([&thread_pool, &strand, f]() {
+                              asio::post(thread_pool, strand.wrap(f));
+                          }));
+            thread_pool.join();
+        }
+
+        template <typename OutputIt, typename Size, typename Generator>
+        static void generate_n(OutputIt first, Size count, Generator g) {
+            asio::thread_pool thread_pool;
+            auto strand = make_shared(thread_pool);
+            std::generate_n(first,
+                          strand.wrap([&thread_pool, &strand, f]() {
+                              asio::post(thread_pool, strand.wrap(f));
+                          }));
+            thread_pool.join();
+        }
+    }
 } // namespace OpenGA
